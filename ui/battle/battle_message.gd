e93fc8e689ef4e4f5707be3e0e94a1b9babeb399
mzytnicki <34034101+mzytnicki@users.noreply.github.com>
2026-09-14 17:56:38 +0000

    Deploying to main from @ mzytnicki/lancelot@fcc47359cf4e1390f0b71d9e12c2203c29916ecb 🚀

--- ui/battle/battle_message.gd
+++
@@ -1,9 +0,0 @@
-extends PanelContainer
-
-@onready var _label: Label = $MarginContainer/Label
-
-func set_message(text: String) -> void:
-	visible = true
-	_label.text = text
-	await get_tree().create_timer(2).timeout
-	visible = false
